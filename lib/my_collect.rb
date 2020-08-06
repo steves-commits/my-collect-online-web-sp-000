4a98f72e98d2393297d896db219fe0fef1a81b31
Steven Topazio <sat@northpointprinting.com>
2020-08-06 19:00:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
 def my_collect(collection)
   i = 0 
-  new collection = []
+  new_collection = []
   while i < collection.length 
-  new collection << yield(collection[i])
+  new_collection << yield(collection[i])
 i += 1 
 end
 new_collection
